67026d392848db03f1399471ab944fdbcd89a58e
Matthew <matthew.kolesnik@gmail.com>
2026-03-25 17:15:28 -0400

    finnaly connected to database

--- Work/connect.php
+++ Work/connect.php
@@ -1,12 +1,16 @@
 <?php
-try {
-    $dbh = new PDO(
-        "mysql:host=localhost;dbname=kolesnm_db;charset=utf8mb4",
-        "kolesnm_local",
-        "2oHN7n5-3S93jd_1"
-    );
+$host = "127.0.0.1"; 
+$db   = "kolesnm_db";
+$user = "kolesnm_local";
+$pass = "2oHN7n5-3S93jd_1";
+$charset = 'utf8mb4';
 
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Error connecting to the database: " . $e->getMessage());
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+     $dbh = new PDO($dsn, $user, $pass);
+     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+     die("DATABASE CONNECTION FAILED: " . $e->getMessage());
 }
+?>
